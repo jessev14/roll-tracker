2293f03e32823f784192d71d32c9402b78464aa5
jessev14 <jessevo321@gmail.com>
2023-12-15 10:58:16 -0500

    fix user comparison

--- scripts/roll-tracker.js
+++ scripts/roll-tracker.js
@@ -645,10 +645,15 @@ class RollTrackerData {
     static async generalComparison() {
         let allStats = {}
         for (let user of game.users) {
-            if (game.users.get(user.id)?.getFlag(RollTracker.ID, RollTracker.FLAGS.SORTED)) {
-                const rolls = this.getUserRolls(user.id)?.sorted
-                allStats[`${user.id}`] = await this.calculate(rolls)
-            }
+            let rolls;
+            if (game.settings.get(RollTracker.ID, RollTracker.SETTINGS.PF2E.TRACK_ROLL_TYPE)) {
+                rolls = [];
+                for (const [k, v] of Object.entries(this.getUserRolls(user.id))) {
+                    if (!['user', 'export', 'streak'].includes(k)) rolls.push(v);
+                }
+            } else rolls = this.getUserRolls(user.id)?.sorted;
+            allStats[`${user.id}`] = await this.calculate(rolls)
+
         }
         // highest/lowest of
 
